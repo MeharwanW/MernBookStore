f54d85e3eda7b0b6279a28774a4bd39b57041df3
Meharwan Wadhwani <108510260+MeharwanW@users.noreply.github.com>
2024-04-25 11:45:04 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Hero></Hero>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
